c5f222c3154d33231db6f368a7f1e20def3b237d
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-03-30 09:50:12 +1100

    Ruby fix

--- iascable.rb
+++ iascable.rb
@@ -1,4 +1,4 @@
-class HomebrewIascable < Formula
+class Iascable < Formula
   desc "IasCable is a node/typescript library to build installable component infrastructure components from a catalog of available modules"
   homepage ""
   url "https://github.com/cloud-native-toolkit/iascable/releases/download/2.11.0/iascable-macos"
